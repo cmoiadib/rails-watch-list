bf94227b4f04aa386a92addef75486166fd88d8c
parwez amini <cmoiadib@gmail.com>
2024-11-14 17:10:50 +0100

    Add controllers and models

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,14 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require "open-uri"
+require "json"
+
+url = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=8d7f04a36275e204d04a7c56a5f1ce0e"
+movie_list = URI.open(url).read
+movies = JSON.parse(movie_list)["results"]
+
+puts "Creating movies"
+
+movies.each do |movie|
+  Movie.create(title: movie["original_title"], overview: movie["overview"], poster_url: "https://image.tmdb.org/t/p/original#{movie["poster_path"]}", rating: movie["vote_average"].round(1))
+end
+
+puts "#{Movie.count} movies created!"
